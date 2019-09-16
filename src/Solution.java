82dd94d707770bee43ecc1fa13f7e9608db0a6ea
mkaterina <52751036+mkaterina@users.noreply.github.com>
2019-09-16 16:59:37 +0300

    Update Solution.java
    10 тысяч удалений и вставок

--- src/Solution.java
+++ src/Solution.java
@@ -1,56 +1,73 @@
+/*Для arrayList и linkedList провести 10 тысяч вставок, удалений, а также вызовов get и set.
+        Требования:
 
-import java.util.HashMap;
-import java.util.Map;
-
-/*
-Коллекция Map из котов
-Есть класс Cat с полем имя (name, String).
-Создать коллекцию Map<String, Cat> (реализация HashMap).
-Добавить в коллекцию 10 котов, в качестве ключа использовать имя кота.
-Вывести результат на экран, каждый элемент с новой строки.
-
-Требования:
-Программа не должна считывать значения с клавиатуры.
-В методе addCatsToMap() объяви переменную коллекции Map с типом элементов String, Cat и сразу проинициализируй ee.
-Метод addCatsToMap() должен добавлять в коллекцию всех котов из массива String[] cats, согласно условию.
-Программа должна выводить содержимое коллекции на экран, каждую пару через дефис и с новой строки.
+        Программа не должна выводить текст на экран.
+        Программа не должна считывать значения с клавиатуры.
+        Класс Solution должен содержать только 5 методов.
+        Метод insert10000(List list) должен вставлять 10 тысяч элементов в список.
+        Метод get10000(List list) должен вызывать 10 тысяч раз get у списка.
+        Метод set10000(List list) должен вызывать 10 тысяч раз set у списка.
+        Метод remove10000(List list) должен удалять 10 тысяч элементов из списка.
+        10 тысяч удалений и вставок
 */
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+
+
+
+
 public class Solution {
-    public static void main(String[] args) throws Exception {
-        String[] cats = new String[]{"васька", "мурка", "дымка", "рыжик", "серый", "снежок", "босс", "борис", "визя", "гарфи"};
+    public static void main(String[] args) {
+        // ArrayList
+        ArrayList arrayList = new ArrayList();
+        insert10000(arrayList);
+        get10000(arrayList);
+        set10000(arrayList);
+        remove10000(arrayList);
 
-        Map<String, Cat> map = addCatsToMap(cats);
-
-        for (Map.Entry<String, Cat> pair : map.entrySet()) {
-            System.out.println(pair.getKey() + " - " + pair.getValue());
-        }
+        // LinkedList
+        LinkedList linkedList = new LinkedList();
+        insert10000(linkedList);
+        get10000(linkedList);
+        set10000(linkedList);
+        remove10000(linkedList);
     }
 
-
-    public static Map<String, Cat> addCatsToMap(String[] cats) {
-        HashMap<String, Cat> map = new HashMap<>();
-        for (int i = 0; i < cats.length; i++) {
-            Cat cat = new Cat(cats[i]);
-            map.put(cat.name, cat);
-        }
-        return map;
+    public static void insert10000(List list) {
+        for (int i = 0; i < 10000; i++) {
+            list.add(0, new Object());
         }
         //напишите тут ваш код
 
-
-
-
-    public static class Cat {
-        String name;
-
-        public Cat(String name) {
-            this.name = name;
-        }
-
-        @Override
-        public String toString() {
-            return name != null ? name.toUpperCase() : null;
-        }
     }
+
+    public static void get10000(List list) {
+        for (int i = 0; i < 10000; i++) {
+            list.get(0);
+        }
+        //напишите тут ваш код
+
+    }
+
+    public static void set10000(List list) {
+        for (int i = 0; i < 10000; i++) {
+            list.set(0, new Object());
+        }
+        //напишите тут ваш код
+
+    }
+
+    public static void remove10000(List list) {
+        for (int i = 0; i < 10000; i++) {
+            list.remove(0) ;
+        }
+        //напишите тут ваш код
+
+    }
+    //напишите тут ваш код
 }
+
+
