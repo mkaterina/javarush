f9b11845f96936de750c52840632b2fcc019e54b
mkaterina <52751036+mkaterina@users.noreply.github.com>
2019-09-20 19:14:21 +0300

    Update Solution.java
    Создать массив на 20 чисел.
    Заполнить его числами с клавиатуры.
    Вывести пять наибольших чисел.
    Каждое значение с новой строки.
    
    Требования:
    Программа должна выводить числа на экран.
    Программа должна считывать значения с клавиатуры.
    Класс Solution должен содержать два метода.
    Метод sort() должен сортировать массив чисел от большего к меньшему.
    Метод main() должен вызывать метод sort().
    Программа должна выводить пять наибольших чисел массива. Каждое значение с новой строки.

--- src/Solution.java
+++ src/Solution.java
@@ -1,47 +1,52 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collections;
+
 /*
-1. Ввести с клавиатуры число N.
-2. Считать N целых чисел и заполнить ими список - метод getIntegerList.
-3. Найти минимальное число среди элементов списка - метод getMinimum.
+Пять победителей
+Создать массив на 20 чисел.
+Заполнить его числами с клавиатуры.
+Вывести пять наибольших чисел.
+Каждое значение с новой строки.
 
 Требования:
-Программа должна выводить текст на экран.
+Программа должна выводить числа на экран.
 Программа должна считывать значения с клавиатуры.
-Класс Solution должен содержать только три метода.
-Метод getIntegerList() должен считать с клавиатуры число N, потом вернуть список размером N элементов, заполненный числами с клавиатуры.
-Метод getMinimum() должен вернуть минимальное число среди элементов списка.
-Метод main() должен вызывать метод getIntegerList().
-Метод main() должен вызывать метод getMinimum().
- */
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
-
-/* 
-Минимальное из N чисел
+Класс Solution должен содержать два метода.
+Метод sort() должен сортировать массив чисел от большего к меньшему.
+Метод main() должен вызывать метод sort().
+Программа должна выводить пять наибольших чисел массива. Каждое значение с новой строки.
 */
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        List<Integer> integerList = getIntegerList();
-        System.out.println(getMinimum(integerList));
-    }
-
-    public static int getMinimum(List<Integer> array) {
-        return Collections.min(array);
-        // Найти минимум тут
-
-    }
-
-    public static List<Integer> getIntegerList() throws IOException {
-        BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
-
-        List<Integer> list = new ArrayList<Integer>();
-        int a = Integer.parseInt(rd.readLine());
-        for (int i=0; i<a;i++) {
-           list.add(Integer.parseInt(rd.readLine()));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int[] array = new int[20];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Integer.parseInt(reader.readLine());
         }
-        // Создать и заполнить список тут
-        return list;
+
+        sort(array);
+
+        System.out.println(array[0]);
+        System.out.println(array[1]);
+        System.out.println(array[2]);
+        System.out.println(array[3]);
+        System.out.println(array[4]);
+    }
+
+    public static void sort(int[] array) {
+        for (int i=0; i<array.length;i++) array[i] *=-1;
+
+        Arrays.sort(array);
+
+        for (int i=0; i<array.length;i++) array[i] *=-1;
+
+
+
+        //напишите тут ваш код
     }
 }
